7174c7a9c53cb820a7f9d129cd88fefbdb718cf3
beatrizana2004 <143664420+beatrizana2004@users.noreply.github.com>
2024-06-18 19:49:17 -0300

    Add files via upload
    fim

--- atividade.js
+++ atividade.js
@@ -1,17 +1,18 @@
 const body = document.querySelector('body');
 body.style.height = '100vh';
 body.style.overflow = 'hidden';
-body.style.backgroundColor = 'violet';
+body.style.backgroundColor = '#79451d';
 body.style.display = 'flex';
 body.style.flexDirection = 'column';
 body.style.justifyContent = 'space-between';
 body.style.alignItems = 'center';
+body.style.margin = '0px'
 
 const header = document.createElement('header');
 header.style.width = '150%';
 header.style.height = '8%';
 header.style.display = 'flex';
-header.style.backgroundColor = 'blue';
+header.style.backgroundColor = '#2f2c2b';
 body.appendChild(header);
 
 const div2 = document.createElement('div');
@@ -21,12 +22,12 @@ div2.style.display = 'flex';
 div2.style.justifyContent = 'space-evenly';
 div2.style.flexDirection = 'column';
 div2.style.alignItems = 'center';
-div2.style.backgroundColor = 'red';
-div2.style.borderRadius = '50px';
+div2.style.backgroundColor = '#ffe3b3';
 body.appendChild(div2);
 
 const textoDiv = document.createElement('h1');
 textoDiv.textContent = 'Atividade Programação Web';
+textoDiv.style.backgroundColor = 'white'
 div2.appendChild(textoDiv);
 
 const nome = document.createElement('input');
@@ -36,7 +37,6 @@ nome.placeholder = 'Digite seu nome';
 nome.style.display = 'flex';
 nome.style.justifyContent = 'center';
 nome.style.backgroundColor = 'white';
-nome.style.borderRadius = '50px';
 div2.appendChild(nome);
 
 const nota1 = document.createElement('input');
@@ -47,7 +47,7 @@ nota1.placeholder = 'Digite a primeira nota';
 nota1.style.display = 'flex';
 nota1.style.justifyContent = 'center';
 nota1.style.backgroundColor = 'white';
-nota1.style.borderRadius = '50px';
+
 div2.appendChild(nota1);
 
 const nota2 = document.createElement('input');
@@ -58,7 +58,6 @@ nota2.placeholder = 'Digite a segunda nota';
 nota2.style.display = 'flex';
 nota2.style.justifyContent = 'center';
 nota2.style.backgroundColor = 'white';
-nota2.style.borderRadius = '50px';
 div2.appendChild(nota2);
 
 const nota3 = document.createElement('input');
@@ -69,7 +68,6 @@ nota3.placeholder = 'Digite a terceira nota';
 nota3.style.display = 'flex';
 nota3.style.justifyContent = 'center';
 nota3.style.backgroundColor = 'white';
-nota3.style.borderRadius = '50px';
 div2.appendChild(nota3);
 
 const bt = document.createElement('button');
@@ -78,7 +76,6 @@ bt.style.width = '200px';
 bt.style.height = '30px';
 bt.style.backgroundColor = 'white';
 bt.style.textAlign = 'center';
-bt.style.borderRadius = '50px';
 div2.appendChild(bt);
 
 const resultado = document.createElement('p');
@@ -92,20 +89,16 @@ bt.addEventListener('click', function () {
     let nota2Value = parseFloat(nota2.value);
     let nota3Value = parseFloat(nota3.value);
 
-    if (isNaN(nota1Value) || isNaN(nota2Value) || isNaN(nota3Value)) {
-        resultado.textContent = 'Por favor, insira valores numéricos válidos para as notas.';
-    } else {
         let med = (nota1Value + nota2Value + nota3Value) / 3;
-        let status = med >= 6 ? 'aprovado' : 'reprovado';
-        resultado.textContent = `Olá, ${nomeValue}, sua média é: ${med.toFixed(2)}. Você está ${status}.`;
+        resultado.textContent = `Olá, ${nomeValue}, sua média é: ${med.toFixed(2)}.`;
     }
-});
+);
 
 const footer = document.createElement('footer');
 footer.style.width = '150%';
 footer.style.height = '8%';
 footer.style.display = 'flex';
-footer.style.backgroundColor = 'blue';
+footer.style.backgroundColor = '#2f2c2b';
 body.appendChild(footer);
 
  
